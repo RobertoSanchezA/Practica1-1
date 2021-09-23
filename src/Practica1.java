fe4e45493d082f47b857ff6ce94d593c14942d00
RobertoSanchezA <usr2266@salesianos-lacuesta.net>
2021-09-23 21:37:19 +0200

    Feature: Lectura del fichero y generar ArrayList con los datos

--- src/Practica1.java
+++ src/Practica1.java
@@ -3,21 +3,32 @@ import java.io.File;
 import java.io.FileReader;
 
 import java.io.*;
+import java.util.ArrayList;
+
 public class Practica1 {
     public static void main(String[] args) {
-
+        //leer el archivo con los registros
         File archivo = null;
         FileReader fr = null;
         BufferedReader br = null;
-        String alumnosSeleccionados [];
+
+        // genero dos arrayLists, uno con todos los alumnos
+        // y uno donde guardo los seleccionados
+
+        ArrayList<String> arrayAlumnos = new ArrayList<String>();
+        ArrayList<String> alumnosRandom = new ArrayList<String>();
         try{
             archivo = new File("D:\\2º DAM\\P. multimedia y dispositivos moviles\\Listado.csv");
             fr = new FileReader(archivo);
             br = new BufferedReader(fr);
 
+            // guardo en el arrayList todos los alumnos
             String linea;
-            while ((linea = br.readLine()) != null)
-                System.out.println(linea);
+            while ((linea = br.readLine()) != null){
+                arrayAlumnos.add(linea);
+            }
+
+
 
         } catch (Exception error){
             error.printStackTrace();
